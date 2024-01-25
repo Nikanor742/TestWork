b1ae194c56ce3c7ec0e09aeca15104e56b502410
Nikanor <nik14kg@gmail.com>
2024-01-25 15:31:30 +0700

    Update FoodTrasher.cs

--- Assets/Scripts/Kitchen/FoodTrasher.cs
+++ Assets/Scripts/Kitchen/FoodTrasher.cs
@@ -11,17 +11,28 @@ namespace CookingPrototype.Kitchen {
 		FoodPlace _place = null;
 		float     _timer = 0f;
 
+		const float _doubleTapTime = 0.3f;
+
 		void Start() {
 			_place = GetComponent<FoodPlace>();
-			_timer = Time.realtimeSinceStartup;
 		}
 
 		/// <summary>
 		/// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
 		/// </summary>
 		[UsedImplicitly]
-		public void TryTrashFood() {
-			Debug.Log("TryTash");
+		public void TryTrashFood(){
+			if ( _place.CurFood.CurStatus == Food.FoodStatus.Overcooked ){
+				if(_timer == 0 ) {
+					_timer = Time.realtimeSinceStartup;
+				}
+				else {
+					if( Time.realtimeSinceStartup - _timer < _doubleTapTime ) {
+						_place.ExtractFood();
+					}
+					_timer = 0;
+				}
+			}
 		}
 	}
 }
